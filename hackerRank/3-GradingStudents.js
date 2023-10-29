acff626c5ac75189667b7a56cc58bcc19539d570
Patrick O'Brien <obrienp2321@gmail.com>
2023-10-29 18:42:05 +0100

    freeCodeCamp/5 Algorithms problem 17-19

--- hackerRank/3-GradingStudents.js
+++ hackerRank/3-GradingStudents.js
@@ -6,7 +6,7 @@ function convertGrades(grades) {
   for (let i = 0; i < grades.length; i++) {
     let remainder = grades[i] % 5;
 
-    if (grades[i] < 38 || grades[i] >= 97 || remainder < 3) {
+    if (grades[i] < 38  || remainder < 3) {
       updatedGrades.push(grades[i]);
     } else {
       let toAdd = 5 - remainder;
@@ -18,4 +18,4 @@ function convertGrades(grades) {
   return updatedGrades;
 }
 
-console.log(convertGrades([38, 45, 28, 97, 88, 94, 78, 40, 67, 100]));
+console.log(convertGrades([38, 45, 28, 98, 88, 94, 78, 40, 67, 100]));
